d5c408a1c6686f4d9fa72b82e4194c7678799a35
P Sai Deekshith <56923148+deekshith39@users.noreply.github.com>
2020-12-30 14:28:12 +0530

    OE questions

---
+++ Advanced DataStructures/Open Ended Question - 1/openEndedQuestion1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    cout << "Enter the number of elements : " ;
+    int n;
+    cin >> n;
+    int a[n];
+    cout << "Enter the elements : " << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cin >> a[j];
+    }
+    sort(a, a+n);   
+    cout << "Output is : " << endl;
+    for (int j = 0; j < n-2; j++)
+    {
+        cout << a[j] << " ";
+    }
+    cout << endl;             
+    
+    
+}
---
+++ Advanced DataStructures/Open Ended Question - 2/openEndedQuestion2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printUnique(int L, int R) {
+    for (int i = L; i <= R; i++)
+    {
+        int n = i;
+        int rem;
+        bool again[10] = {false};
+        while (n) {
+            rem = n % 10;
+            if (again[rem])
+                break;
+            again[rem] = true;
+            n /= 10; 
+        }
+        if (n == 0)
+            cout << i << " ";
+    }    
+}
+
+int main() {
+    int L, R;
+    cout << "Enter the limits : "; 
+    cin >> L >> R;
+    cout << "Output is : " << endl;
+    printUnique(L, R);
+}
